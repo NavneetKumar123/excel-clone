2efae601c950a1ac2004f6f2236b0ed370ab0832
Navneet kumar <navneetcool20@gmail.com>
2021-05-20 10:32:31 +0530

    Test commit

--- activity/script1.js
+++ activity/script1.js
@@ -1,4 +1,4 @@
-let btnContainer = document.querySelector(".add-sheet_btn_container");
+let btnContainer = document.querySelector(".add-sheet_btn-container");
 let sheetList=document.querySelector(".sheet-list");
 let firstSheet = document.querySelector(".sheet");
 firstSheet.addEventListener("click",handleSheet)
@@ -11,7 +11,7 @@ btnContainer.addEventListener("click",function (){
     Lastidx = Number(Lastidx);
     let NewSheet = document.createElement("div");
     NewSheet.setAttribute("class","sheet");
-    NewSheet.setAttribute("idx" `${Lastidx + 1}`);
+    NewSheet.setAttribute("idx", `${Lastidx + 1}`); // yha idx ke baad comma chhod diyaa tha ab sahi chal rha h
     NewSheet.innerText = `sheet ${Lastidx + 2}`;
     sheetList.appendChild(NewSheet);
     for( let i=0; i<AllSheets.length; i++){
@@ -21,6 +21,25 @@ btnContainer.addEventListener("click",function (){
         //new sheet crate
         NewSheet.addEventListener("click", handleSheet)
 })
+
+// btnContainer.addEventListener("click", function () {
+//     // create sheet 
+//     let AllSheets = document.querySelectorAll(".sheet");
+//     let lastSheet = AllSheets[AllSheets.length - 1];
+//     let Lastidx = lastSheet.getAttribute("idx");
+//     Lastidx = Number(Lastidx);
+//     let Newsheet = document.createElement("div");
+//     Newsheet.setAttribute("class", "sheet");
+//     Newsheet.setAttribute("idx", `${Lastidx + 1}`);
+//     Newsheet.innerText = `Sheet ${Lastidx + 2}`;
+//     sheetList.appendChild(Newsheet);
+//     for (let i = 0; i < AllSheets.length; i++) {
+//         AllSheets[i].classList.remove("active-btn");
+//     }
+//     Newsheet.classList.add("active-btn");
+//     // new sheet create 
+//     Newsheet.addEventListener("click", handleSheet)
+// })
 function handleSheet(e) {
     let sheet = e.currentTarget;
     let AllSheets = document.querySelectorAll(".sheet");
